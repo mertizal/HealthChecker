0433e765033bc7dce72566d318c57b7a318fbadb
mert izal <mert.915@hotmail.com>
2023-08-26 16:44:30 +0300

    denemev6

--- src/test/java/com/example/HealthChecker/HealthCheckerApplicationTests.java
+++ src/test/java/com/example/HealthChecker/HealthCheckerApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.HealthChecker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 
 @SpringBootTest
+@ContextConfiguration
 class HealthCheckerApplicationTests {
 
 	@Test
