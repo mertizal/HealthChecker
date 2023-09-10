2e9bf3eb67423de0ee3c71fc60e77ba9088fbe0a
mert izal <mert.915@hotmail.com>
2023-09-10 23:36:23 +0300

    Add test env

--- src/test/java/com/example/HealthChecker/HealthCheckerApplicationTests.java
+++ src/test/java/com/example/HealthChecker/HealthCheckerApplicationTests.java
@@ -2,10 +2,12 @@ package com.example.HealthChecker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HealthCheckerApplicationTests {
 
 	@Test
