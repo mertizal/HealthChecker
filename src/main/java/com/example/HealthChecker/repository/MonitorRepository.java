5030e264e063710900ee46cd2c9526d90be3d8c9
mert izal <mert.915@hotmail.com>
2023-09-10 22:13:00 +0300

    Filter monitors using sql

--- src/main/java/com/example/HealthChecker/repository/MonitorRepository.java
+++ src/main/java/com/example/HealthChecker/repository/MonitorRepository.java
@@ -3,8 +3,13 @@ package com.example.HealthChecker.repository;
 import com.example.HealthChecker.Models.Monitor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MonitorRepository extends JpaRepository<Monitor,Long> {
 
+    @Query(value = "SELECT * FROM monitor WHERE last_run_time is null or last_run_time + \"interval\" * interval '1 second' <= now() + interval '3 hour'", nativeQuery = true)
+    List<Monitor> FiterBlaBla();
 
 }
