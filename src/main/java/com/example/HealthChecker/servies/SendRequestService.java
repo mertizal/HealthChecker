a375b5efc3e52425c108b7c0cca81803ca0c4e99
mert izal <mert.915@hotmail.com>
2023-10-07 20:49:19 +0300

    v2

--- src/main/java/com/example/HealthChecker/servies/SendRequestService.java
+++ src/main/java/com/example/HealthChecker/servies/SendRequestService.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
